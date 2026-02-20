bff0d86b777e766afb573e13f7cc9e877f27e6a1
sahith <129191778+sahith14@users.noreply.github.com>
2026-02-21 03:35:44 +0530

    Create ProtectedRoute.jsx

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return null;
+  if (!user || user.email !== "zenomedia.work@gmail.com")
+    return <Navigate to="/" />;
+
+  return children;
+}
