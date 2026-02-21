97f3ddc5a2daf896190f09b74498d6452270fe65
sahith <129191778+sahith14@users.noreply.github.com>
2026-02-21 05:52:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './routes/Home'
-import About from './routes/About'
-import Contact from './routes/Contact'
-import Shop from './routes/Shop'
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Shop from './pages/Shop';
+import Contact from './pages/Contact';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+import Downloads from './pages/Downloads';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminRoute from './components/AdminRoute';
 
-export default function App() {
+function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+          <Route
+            path="downloads"
+            element={
+              <ProtectedRoute>
+                <Downloads />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="admin-7x91k"
+            element={
+              <AdminRoute>
+                <Admin />
+              </AdminRoute>
+            }
+          />
+        </Route>
       </Routes>
-    </Router>
-  )
+    </BrowserRouter>
+  );
 }
+
+export default App;
