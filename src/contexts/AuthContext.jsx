aab9eac6806431bdf67490d7f3ab4e2ec8c43ca6
sahith <129191778+sahith14@users.noreply.github.com>
2026-02-21 05:55:46 +0530

    Create AuthContext.jsx

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,43 @@
+// src/contexts/AuthContext.jsx
+import { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase/config';
+import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    return signOut(auth);
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
