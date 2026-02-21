780307a68a8dce74984ca6c13240781a6eb1c7a5
sahith <129191778+sahith14@users.noreply.github.com>
2026-02-22 01:20:54 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'serif': ['Playfair Display', 'serif'],
+        'sans': ['Inter', 'sans-serif'],
+      },
       colors: {
         gold: '#CFB53B',
       },
